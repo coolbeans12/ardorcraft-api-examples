9c2c9a91c839cbc21684c515539f3b1589d92d76
rikardherlitz@gmail.com <rikardherlitz@gmail.com@1b0c645f-27d7-2e08-1c60-ff43a117bbd0>
2011-05-17 12:09:07 +0000

    Fixed some doc.

--- trunk/src/com/ardorcraft/player/PlayerWithPhysics.java
+++ trunk/src/com/ardorcraft/player/PlayerWithPhysics.java
@@ -1,3 +1,4 @@
+
 package com.ardorcraft.player;
 
 import com.ardor3d.input.logical.LogicalLayer;
@@ -7,90 +8,91 @@ import com.ardorcraft.control.FlyControl;
 import com.ardorcraft.control.WalkControl;
 import com.ardorcraft.world.BlockWorld;
 
+/**
+ * A player implementation with walk/fly modes and gravity and jumping.
+ */
 public class PlayerWithPhysics extends PlayerWithCollision {
-	private boolean jump = false;
-	private double time;
-	private final double stepTime = 1.0 / 100.0;
-	private boolean walking = false;
-	private final Vector3 damping = new Vector3(0.85, 1.0, 0.85);
-	private final double gravity = -0.3;
-	private final double jumpForce = 9;
-	private final double speed = 0.9;
+    private boolean jump = false;
+    private double time;
+    private final double stepTime = 1.0 / 100.0;
+    private boolean walking = false;
+    private final Vector3 damping = new Vector3(0.85, 1.0, 0.85);
+    private final double gravity = -0.3;
+    private final double jumpForce = 9;
+    private final double speed = 0.9;
 
-	private final WalkControl walkControl;
-	private final FlyControl flyControl;
+    private final WalkControl walkControl;
+    private final FlyControl flyControl;
 
-	public PlayerWithPhysics(final LogicalLayer logicalLayer) {
-		flyControl = FlyControl.setupTriggers(this, logicalLayer,
-				Vector3.UNIT_Y, false);
-		// flyControl.setMoveSpeed(50);
-		flyControl.disable();
-		walkControl = WalkControl.setupTriggers(this, logicalLayer,
-				Vector3.UNIT_Y, false);
-	}
+    public PlayerWithPhysics(final LogicalLayer logicalLayer) {
+        flyControl = FlyControl.setupTriggers(this, logicalLayer, Vector3.UNIT_Y, false);
+        // flyControl.setMoveSpeed(50);
+        flyControl.disable();
+        walkControl = WalkControl.setupTriggers(this, logicalLayer, Vector3.UNIT_Y, false);
+    }
 
-	@Override
-	public void update(final BlockWorld blockScene, final ReadOnlyTimer timer) {
-		final double tpf = timer.getTimePerFrame();
+    @Override
+    public void update(final BlockWorld blockScene, final ReadOnlyTimer timer) {
+        final double tpf = timer.getTimePerFrame();
 
-		time += tpf;
+        time += tpf;
 
-		int ticks = 0;
-		while (time > stepTime) {
-			final double step = stepTime;
+        int ticks = 0;
+        while (time > stepTime) {
+            final double step = stepTime;
 
-			getVelocity().addLocal(getAcceleration().multiply(speed, null));
-			if (walking) {
-				getVelocity().addLocal(0, gravity, 0);
-				if (jump && isOnGround()) {
-					getVelocity().addLocal(0, jumpForce, 0);
-				}
-			}
-			getPosition().addLocal(getVelocity().multiply(step, null));
+            getVelocity().addLocal(getAcceleration().multiply(speed, null));
+            if (walking) {
+                getVelocity().addLocal(0, gravity, 0);
+                if (jump && isOnGround()) {
+                    getVelocity().addLocal(0, jumpForce, 0);
+                }
+            }
+            getPosition().addLocal(getVelocity().multiply(step, null));
 
-			getVelocity().multiplyLocal(damping);
+            getVelocity().multiplyLocal(damping);
 
-			// Update collision etc
-			super.update(blockScene, timer);
+            // Update collision etc
+            super.update(blockScene, timer);
 
-			time -= stepTime;
-			ticks++;
-			jump = false;
-		}
-		if (ticks > 0) {
-			getAcceleration().set(0, 0, 0);
-		}
-		jump = false;
-	}
+            time -= stepTime;
+            ticks++;
+            jump = false;
+        }
+        if (ticks > 0) {
+            getAcceleration().set(0, 0, 0);
+        }
+        jump = false;
+    }
 
-	double t = 0;
+    double t = 0;
 
-	public void jump() {
-		if (!jump && isOnGround()) {
-			jump = true;
-		}
-	}
+    public void jump() {
+        if (!jump && isOnGround()) {
+            jump = true;
+        }
+    }
 
-	public boolean isWalking() {
-		return walking;
-	}
+    public boolean isWalking() {
+        return walking;
+    }
 
-	public void setWalking(final boolean walking) {
-		this.walking = walking;
-		if (isWalking()) {
-			walkControl.enable();
-			flyControl.disable();
-		} else {
-			walkControl.disable();
-			flyControl.enable();
-		}
-	}
+    public void setWalking(final boolean walking) {
+        this.walking = walking;
+        if (isWalking()) {
+            walkControl.enable();
+            flyControl.disable();
+        } else {
+            walkControl.disable();
+            flyControl.enable();
+        }
+    }
 
-	public WalkControl getWalkControl() {
-		return walkControl;
-	}
+    public WalkControl getWalkControl() {
+        return walkControl;
+    }
 
-	public FlyControl getFlyControl() {
-		return flyControl;
-	}
+    public FlyControl getFlyControl() {
+        return flyControl;
+    }
 }
